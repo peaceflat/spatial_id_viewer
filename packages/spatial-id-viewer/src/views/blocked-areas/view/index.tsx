707dabbe6b7aa21efe2a57596383df9d57ab69f1
Thiniraxz <thiniragenuka@gmail.com>
2024-09-09 23:40:16 +0530

    Bug fix

--- packages/spatial-id-viewer/src/views/blocked-areas/view/index.tsx
+++ packages/spatial-id-viewer/src/views/blocked-areas/view/index.tsx
@@ -128,7 +128,14 @@ const useLoadModels = () => {
   const authInfo = useLatest(useAuthInfo((s) => s.authInfo));
 
   const loadModels = useCallback(async (displayDetails: DisplayDetails) => {
-    displayDetails.figure.identification.ID = displayDetails.figure.identification.ID.toString();
+    const spatialID = displayDetails.figure.identification.ID;
+    const newSpatialID = new SpatialId(
+      spatialID.z,
+      spatialID.f,
+      spatialID.x,
+      spatialID.y
+    ).toString();
+    displayDetails.figure.identification.ID = newSpatialID;
     const areas = await processBlockedAreas(
       getBlockedAreas({
         baseUrl: apiBaseUrl,
